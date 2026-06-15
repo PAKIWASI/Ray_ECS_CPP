2781eccc39d167136d04cedcbaa6a2ef301cb151
PAKIWASI <ullahwasi26@gmail.com>
2026-06-16 00:47:30 +0500

    new shi

--- ecs/ecs.hpp
+++ ecs/ecs.hpp
@@ -51,59 +51,50 @@ using ComponentType = u8;
 // Entity Manager
 // ===============
 
-// hands out IDs to new entities
+// creates and deletes entities
+// gives out ids and takes them back
 class EntityManager
 {
   private:
-    Entity next_id = 0;         // goes from 0 to MAX_ENTITIES
+    // no startup cost of pre filling container with ids
+    Entity next_id = 0;
     // when an entity dies, it's id goes here for reuse
     std::vector<Entity> free_ids;
+
+    std::array<Signature, MAX_ENTITIES> signatures;
   public:
 
     [[nodiscard]] auto create_entity() -> Entity
     {
-        Entity next = next_id++;
-        assert(next < MAX_ENTITIES && "MAX_ENTITIES reached");
-        return next;
+        if (!free_ids.empty()) {
+            Entity recycled = free_ids.back();
+            free_ids.pop_back();
+            return recycled;
+        }
+        assert(next_id < MAX_ENTITIES && "MAX_ENTITIES reached");
+        return next_id++;
     }
 
     void destroy_entity(Entity e)
     {
         // max value of next_id is MAX_ENTITIES
         assert(e < next_id && "Entity out of range");
+        // reset the signature for later use
+        signatures.at(e).reset();
         free_ids.emplace_back(e);
+    }
+
+    void set_component(Entity e, ComponentType c)
+    {
+        signatures.at(e).set(c);
     }
 };
 
 
-// Assigning IDs to each component type
-// =====================================
-
-// global constexpr counter
-// each component type is templated with T
-// when initing each component type, it will grab and id from here the increment
-// TODO: move this to component_manager so we don't have anything global
-inline ComponentType comp_id = 0;
-
-// TODO: can i make this run at compile time completely?
-//
-// when this function is called with different T for the first time, it will
-// init static var and return a new id. all subsequent calls with same T return same id
-template <typename T>
-[[nodiscard]] auto get_component_id(T comp) -> ComponentType
-{
-    // static var inside func initialized only once
-    static ComponentType id = comp_id++;
-
-    assert(id < MAX_COMPONENTS && "max componentypes reached");
-
-    return id;
-}
-
 // Concept: what counts as a component
 template <typename T>
 concept ComponentType_t = std::default_initializable<T> // each comp arr is pre-initialized
-                        && std::movable<T>;             // we move data into/outof the arr
+                       && std::movable<T>;              // we move data into/outof the arr
 
 // Type Erasure
 // =============
@@ -111,10 +102,19 @@ concept ComponentType_t = std::default_initializable<T> // each comp arr is pre-
 // we have a interface with type agnostic methods. container will have that type
 // when we need type specific stuff, we will cast to the correct type
 
+// The Rule of Five warning fires because you declared a destructor but nothing else
+// Since ICompArr is a pure interface not meant to be copied or moved, explicitly delete those
 struct ICompArr
 {
-    virtual ~ICompArr() = default;          // one vtable entry
-    virtual void entity_destroyed() = 0;    // another vtable entry
+    ICompArr()                                   = default;
+    ICompArr(const ICompArr&)                    = delete;
+    auto operator=(const ICompArr&) -> ICompArr& = delete;
+    ICompArr(ICompArr&&)                         = delete;
+    auto operator=(ICompArr&&) -> ICompArr&      = delete;
+    virtual ~ICompArr()                          = default; // one vtable entry
+    // the only reason entity_destroyed is virtual is because ComponentManager holds ICompArr*
+    // and needs to call something through the base pointer without knowing T
+    virtual void entity_destroyed(Entity e)      = 0;       // another vtable entry
 };
 
 
@@ -124,11 +124,74 @@ template <ComponentType_t T>
 class ComponentArray : ICompArr
 {
   private:
-    std::array<T, MAX_ENTITIES> data{};
+    std::array<T, MAX_ENTITIES> data{}; // default initialize
+  public:
+
+    void add_data(Entity e, T comp)
+    {
+        assert(e < MAX_ENTITIES && "Entity out of range");
+        data.at(e) = comp;
+    }
+
+    void remove_data(Entity e)
+    {
+        assert(e < MAX_ENTITIES && "Entity out of range");
+        data.at(e) = T{};  // calls default constructor, correct for any ComponentType_t T
+    }
+
+    [[nodiscard]] auto get_data(Entity e) -> T&
+    {
+        assert(e < MAX_ENTITIES && "Entity out of range");
+        return data.at(e);
+    }
+
+    void entity_destroyed(Entity e) override
+    {
+        remove_data(e);
+    }
+};
+
+
+class ComponentManager
+{
+  private:
+    // we need unique ptr as we can't store an abstract type in an array directly
+    // it's not instantiable, and it would slice anyway
+    std::array<u_ptr<ICompArr>, MAX_COMPONENTS> comp_arrays{};
+    size size = 0;      // number of registered components
+
+    // handing out IDs to each type of component
+    // first call to get_component_id<T>() with a new T yields the next id
+    // all subsequent calls give the same id (static var)
+    // This id determines the index into comp_arrays and also the signature bit position
+    inline static ComponentType next_comp_id = 0;
+
+    template <ComponentType_t T>
+    [[nodiscard]] static auto get_component_id() -> ComponentType
+    {
+        // static var inside func initialized only once
+        static ComponentType id = next_comp_id++;
+
+        assert(id < MAX_COMPONENTS && "MAX_COMPONENTS reached");
+
+        return id;
+    }
 
   public:
 
+    void entity_destroyed(Entity e)
+    {
+        // ERROR: Call to deleted constructor of 'value_type' (aka 'std::unique_ptr<ICompArr>') [ovl_deleted_init] 
+        // for (auto comparr : comp_arrays)
+        // {
+        //
+        // }
 
+        for (u32 i = 0; i < size; i++) {
+            // generic operation, no typecast needed. we do a vtable lookup
+            comp_arrays.at(i)->entity_destroyed(e);
+        }
+    }
 };
 
 
